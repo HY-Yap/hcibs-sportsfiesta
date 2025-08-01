3917f65b19ce944e271cbce701858dc505b3bfeb
Shuhxin <94676786+Shuhxin@users.noreply.github.com>
2025-08-01 17:15:35 +0800

    Update firebase-config.js to sync with login.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,10 +1,16 @@
-// public/js/firebase-config.js
-// Public keys only – safe to commit to Git
-export const firebaseConfig = {
-    apiKey: "AIzaSyDYSel3WBMz0bJEnYIpyAlefHXa-UdEE7Y",
-    authDomain: "hcibs-sportsfiesta.firebaseapp.com",
-    projectId: "hcibs-sportsfiesta",
-    storageBucket: "hcibs-sportsfiesta.firebasestorage.app",
-    messagingSenderId: "1059800316877",
-    appId: "1:1059800316877:web:e5b4a29f8c53f36ca62284",
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYSel3WBMz0bJEnYIpyAlefHXa-UdEE7Y",
+  authDomain: "hcibs-sportsfiesta.firebaseapp.com",
+  projectId: "hcibs-sportsfiesta",
+  storageBucket: "hcibs-sportsfiesta.firebasestorage.app",
+  messagingSenderId: "1059800316877",
+  appId: "1:1059800316877:web:e5b4a29f8c53f36ca62284",
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
